6eb5de47b07e0c53ac9ecb630fb3d36c3eef252a
Jordan Silbert <jordansilbert@dhcp-ccc-1726.redrover.cornell.edu>
2016-11-01 11:31:43 -0400

    index.html

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,12 @@
 <div class="nav_bar">
-            <div id="banner">
-                <h1>Who Doesn't Love Sunsets?</h1>
-            </div>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="secondfile.php">New York's Sunrise and Sunset Data</a></li>
-                <li><a href="http://www.timeanddate.com/sun/">Sunrise and Sunset Calendar</a></li>
-                <li><a href="http://www.cntraveler.com/galleries/2016-05-06/where-to-see-the-worlds-best-sunsets/2">Where to Travel For the Best Sunsets</a></li>
-                <li><a href="https://www.theodysseyonline.com/5-facts-you-didnt-know-about-sunsets">Fun Facts</a></li>
-            </ul>
+        <div id="banner">
+                    <h1>Who Doesn't Love Sunsets?</h1>
         </div>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="secondfile.php">New York's Sunrise and Sunset Data</a></li>
+            <li><a href="http://www.timeanddate.com/sun/">Sunrise and Sunset Calendar</a></li>
+            <li><a href="http://www.cntraveler.com/galleries/2016-05-06/where-to-see-the-worlds-best-sunsets/2">Where to Travel For the Best Sunsets</a></li>
+            <li><a href="https://www.theodysseyonline.com/5-facts-you-didnt-know-about-sunsets">Fun Facts</a></li>
+        </ul>
+    </div>
